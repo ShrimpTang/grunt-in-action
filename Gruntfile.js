5e5273b6dd044d87b6b405062e92fb7dd9bfad6f
Shrimp <moumoon711@gmail.com>
2016-01-06 00:19:54 +0800

    z

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function (grunt) {
     }
     grunt.initConfig({
         config: config,
+        pkg:grunt.file.readJSON('package.json'),
         copy: {
             //方法1--------------------------
             //dist_html: {
@@ -61,7 +62,26 @@ module.exports = function (grunt) {
                 src:'<%= config.dist %>/**/*',
                 filter:'isFile'
             }
+        },
+        uglify:{
+            options:{
+                stripBanners:true,
+                banner:'/* <%= pkg.name %>-<%= pkg.version%>.js <%= grunt.template.today("yyyy-mm-dd")%>  */'
+            },
+            build:{
+                //src:'<%= config.app %>/js/**/*',
+                //dest:'<%= config.dist %>/js/**/*',
+                files:[{
+                    expand:true,
+                    cwd:'<%= config.app%>/js/',
+                    src:'**/*.js',
+                    dest:'<%= config.dist%>/js/',
+                    extDot:'last',
+                    ext:'.min.js'
+                }]
+            }
         }
-
     });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default',['uglify']);
 };
